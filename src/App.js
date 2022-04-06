b20414a792f0affe840fed487cb60c64f6db6156
Javier Salas <javier.salas.991@gmail.com>
2022-04-06 06:32:07 -0300

    guardo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import React from "react";
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Proyectos from "./components/proyectos/Proyectos";
 import Login from "./components/auth/Login";
 import NuevaCuenta from "./components/auth/NuevaCuenta";
+import ProyectoState from "./context/proyectos/proyectoState";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="nueva-cuenta" element={<NuevaCuenta />} />
-        <Route path="proyectos" element={<Proyectos />} />
-      </Routes>
-    </BrowserRouter>
+    <ProyectoState>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="nueva-cuenta" element={<NuevaCuenta />} />
+          <Route path="proyectos" element={<Proyectos />} />
+        </Routes>
+      </BrowserRouter>
+    </ProyectoState>
   );
 }
 
